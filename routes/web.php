65565e30a472d8adba926687b1b236dd419abf0b
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2025-06-20 14:38:32 +0300

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\OIDCController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,3 +14,11 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:api');
+
+Route::get('/.well-known/openid-configuration', [OIDCController::class, 'discovery']);
+Route::middleware('auth:api')->get('/userinfo', [OIDCController::class, 'userinfo']);
+Route::get('/oauth/jwks', [OIDCController::class, 'jwks']);
