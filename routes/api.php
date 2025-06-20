a18050049b52a1c0e8506d36bd563c665c088352
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2025-06-20 17:55:12 +0300

    move token to api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\OIDCController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Laravel\Passport\Http\Controllers\AccessTokenController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
@@ -11,3 +12,8 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/.well-known/openid-configuration', [OIDCController::class, 'discovery']);
 // Route::middleware('auth:api')->get('/userinfo', [OIDCController::class, 'userinfo']);
 // Route::get('/oauth/jwks', [OIDCController::class, 'jwks']);
+
+Route::post('/oauth/token', [
+    'middleware' => 'add.id_token',
+    'uses' => AccessTokenController::class.'@issueToken',
+]);
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\OIDCController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Laravel\Passport\Http\Controllers\AccessTokenController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -23,8 +22,3 @@ Route::get('/user', function (Request $request) {
 Route::get('/.well-known/openid-configuration', [OIDCController::class, 'discovery']);
 Route::middleware('auth:api')->get('/userinfo', [OIDCController::class, 'userinfo']);
 Route::get('/oauth/jwks', [OIDCController::class, 'jwks']);
-
-Route::post('/oauth/token', [
-    'middleware' => 'add.id_token',
-    'uses' => AccessTokenController::class.'@issueToken',
-]);
