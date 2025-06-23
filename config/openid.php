a2ab8e2183a1006791677d86b14ced794d690535
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2025-06-23 12:53:00 +0300

    publish openid config

---
+++ config/openid.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'passport' => [
+
+        /**
+         * Place your Passport and OpenID Connect scopes here.
+         * To receive an `id_token, you should at least provide the openid scope.
+         */
+        'tokens_can' => [
+            'openid' => 'Enable OpenID Connect',
+            'profile' => 'Information about your profile',
+            'email' => 'Information about your email address',
+            'phone' => 'Information about your phone numbers',
+            'address' => 'Information about your address',
+            // 'login' => 'See your login information',
+        ],
+    ],
+
+    /**
+     * Place your custom claim sets here.
+     */
+    'custom_claim_sets' => [
+        // 'login' => [
+        //     'last-login',
+        // ],
+        // 'company' => [
+        //     'company_name',
+        //     'company_address',
+        //     'company_phone',
+        //     'company_email',
+        // ],
+    ],
+
+    /**
+     * You can override the repositories below.
+     */
+    'repositories' => [
+        'identity' => \OpenIDConnect\Repositories\IdentityRepository::class,
+        'scope' => \OpenIDConnect\Repositories\ScopeRepository::class,
+    ],
+
+    /**
+     * The signer to be used
+     * Can be Ecdsa, Hmac or RSA
+     */
+    'signer' => \Lcobucci\JWT\Signer\Rsa\Sha256::class,
+
+    'routes' => [
+        /**
+         * When set to true, this package will expose the OpenID Connect Discovery endpoint.
+         *  - /.well-known/openid-configuration
+         */
+        'discovery' => true,
+        /**
+         * When set to true, this package will expose the JSON Web Key Set endpoint.
+         * - /oauth/jwks
+         */
+        'jwks' => true,
+    ],
+];
