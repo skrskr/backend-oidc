67bc28005ccb11a578f3e408e28cbbc6ebc05882
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2025-06-23 15:24:46 +0300

    create user localy when user register or login

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\OIDCController;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 use Laravel\Passport\Http\Controllers\AccessTokenController;
@@ -22,6 +25,19 @@ Route::post('/oauth/token', [
 Route::post('/immrsa/auth/signup', function (Request $request) {
     $response = Http::post('https://backend.immrsa.io/api/immrsa/auth/signup', $request->all());
 
+    if ($response->successful()) {
+        // Create or update the user locally
+        $data = $request->all();
+        User::updateOrCreate(
+            ['email' => $data['email']],
+            [
+                'name' => $data['firstName'].' '.$data['lastName'],
+                'password' => Hash::make($data['password']), // Store a hash, even if not used
+                'organization' => $data['companyName'] ?? null,
+            ]
+        );
+    }
+
     return response()->json($response->json(), $response->status());
 });
 
@@ -33,6 +49,15 @@ Route::post('/immrsa/auth/verify', function (Request $request) {
 
 Route::post('/immrsa/auth/login', function (Request $request) {
     $response = Http::post('https://backend.immrsa.io/api/immrsa/auth/login', $request->all());
+
+    if ($response->successful()) {
+        $data = $response->json();
+        $userData = $data['user'];
+        $user = \App\Models\User::where('email', $userData['email'])->first();
+        if ($user) {
+            Auth::login($user);
+        }
+    }
 
     return response()->json($response->json(), $response->status());
 });
